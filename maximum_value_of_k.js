a09acbf83f8c24ad495cb2d148f6490909776f0b
Shota-Kudo <64889819+Shota-Kudo@users.noreply.github.com>
2021-06-04 10:14:19 +0900

    Create maximum_value_of_k.js

---
+++ maximum_value_of_k.js
@@ -0,0 +1,44 @@
+function topKElements(intArr,k){
+    //ここから書きましょう
+    buildMaxHeap(intArr);
+    let heapEnd = intArr.length-1;
+    let results = [];
+
+    while(heapEnd >= 0) {
+        let tmp = intArr[heapEnd];
+        intArr[heapEnd] = intArr[0];
+        intArr[0] = tmp;
+        heapEnd--;
+        results.push(intArr.pop());
+        if(results.length >= k) break;
+        maxHeapify(intArr, 0);
+    }
+    return results;
+}
+
+function buildMaxHeap(arr){
+    let mid = parent(arr.length);
+    for(let i = mid; i >= 0; i--) {
+        maxHeapify(arr, i);
+    }
+}
+
+function parent(i) {
+    return Math.floor((i-1)/2);
+}
+
+function maxHeapify(arr, i) {
+    let left = 2*i+1;
+    let right = 2*i+2;
+
+    let biggest = i;
+    if(arr[left] > arr[biggest]) biggest = left;
+    if(arr[right] > arr[biggest]) biggest = right;
+
+    if(biggest !== i) {
+        let tmp = arr[i];
+        arr[i] = arr[biggest];
+        arr[biggest] = tmp;
+        maxHeapify(arr, biggest);
+    }
+}
